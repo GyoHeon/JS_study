c6daeae894084be669c767c687da86adaa3f9db8
GyoHeon Lee <ghl12078@gmail.com>
2021-10-25 04:19:17 +0900

    course 2.4

--- momentum-clone/script.js
+++ momentum-clone/script.js
@@ -1,12 +1,4 @@
-const a = 15;
-const b = 2;
-let myName = `Lee`;
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log(`My name is ` + myName);
-
-myName = `Gyoheon`;
-
-console.log(`My new name is now ` + myName);
+const amIFat = true;
+let amIThin;
+console.log(amIThin);
+console.log(amIFat);
