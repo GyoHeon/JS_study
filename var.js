2a6d9a7bc5e79914122698d447d09622b9778361
GyoHeon Lee <ghl12078@gmail.com>
2021-10-24 14:41:56 +0900

    for, if

--- var.js
+++ var.js
@@ -1,9 +1,7 @@
 let cost = 20;
 
-while (cost < 25) {
-    console.log(`${cost}`);
-    cost++;
-}
-for (let i = 0; i < 5; i++) {
-    console.log(`${i}`);
+for (let i = 0; i < cost; i++) {
+    if (i % 3 === 0) {
+        console.log(`${i}`);
+    }
 }
