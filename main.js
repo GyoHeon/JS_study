3c0018e04efa090d131dadbef178ef5c5960f354
GyoHeon Lee <ghl12078@gmail.com>
2021-10-27 02:14:33 +0900

    fc course 2.9

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-const ulEl = document.querySelector("ul");
-
-for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  li.textContent = `list-${i + 1}`;
-  if ((i + 1) % 2 === 0) {
-    li.addEventListener("click", function () {
-      console.log(li.textContent);
-    });
+function scope() {
+  if (true) {
+    const a = 123;
+    console.log(a);
   }
-  ulEl.appendChild(li);
 }
+scope();
