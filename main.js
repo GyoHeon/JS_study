e3a2d778c1a96133cb3d6f6a16305c8fad0eec41
GyoHeon Lee <ghl12078@gmail.com>
2021-10-27 01:33:23 +0900

    fc course 2.4

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-let a = 2;
-a += 2;
+const a = 1 === 2;
+const b = "AB" === "ABC";
+const c = false;
 
 console.log(a);
+console.log(b);
+console.log(c);
+
+console.log("&& : ", a && b && c);
+console.log("|| : ", a || b || c);
+console.log("! : ", !a);
