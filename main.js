fd1345b50b36c2f0c4cc97f8b8b495b1048815c5
GyoHeon Lee <ghl12078@gmail.com>
2021-10-27 17:59:12 +0900

    fc course 4.3

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const timer = {
-  name: "Heropy!!",
-  timeout: function () {
-    setTimeout(() => {
-      console.log(this.name);
-    }, 2000);
-  },
-};
-timer.timeout();
+class User {
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const heropy = new User("Heropy", "Park");
+
+console.log(heropy);
