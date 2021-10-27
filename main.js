2ea9a216728fd26fd4cf167eb8039bcb324cd7cb
GyoHeon Lee <ghl12078@gmail.com>
2021-10-27 15:11:04 +0900

    fc course 3.1

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-if (0) {
-  console.log(123);
-}
+const sum = function (x, y) {
+  console.log(arguments);
+  return arguments[0] + arguments[1];
+};
+
+const a = sum(1, 3);
+const b = sum(4, 12);
+
+console.log(sum(7, 3));
