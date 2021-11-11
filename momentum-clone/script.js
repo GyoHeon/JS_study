1fa275f3485dd6639877baa75a23e4e99de780b4
GyoHeon Lee <ghl12078@gmail.com>
2021-11-11 23:50:21 +0900

    course 2.15

--- momentum-clone/script.js
+++ momentum-clone/script.js
@@ -1,8 +1,9 @@
-const player = {
-  name: "Gyoheon",
-  sayHello: function (otherPersonsName) {
-    console.log("hello! " + otherPersonsName);
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-player.sayHello("Jonghoon");
+if (isNaN(age)) {
+  console.log("Please write a number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else {
+  console.log("You can drink");
+}
