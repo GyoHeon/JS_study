7ab64346c039bbe5dab16570b5f82bb7d9b521da
GyoHeon Lee <ghl12078@gmail.com>
2021-11-12 16:54:06 +0900

    course 3.1

--- var.js
+++ var.js
@@ -1,7 +1,14 @@
-let cost = 20;
+let a = "1";
+console.log(typeof a);
+a = a * 1;
+console.log(typeof a);
 
-for (let i = 0; i < cost; i++) {
-    if (i % 3 === 0) {
-        console.log(`${i}`);
-    }
-}
+let str = "이렇게 해서 " + a + " 표현합니다.";
+console.log(str);
+str = `이렇게 해서 ${a} 표현합니다.`;
+console.log(str);
+
+let longString = `여러 줄에 걸쳐 작성해야 할 정도로 
+  긴 문자열인데 왜 한 줄에 다 적으면 안되냐면 
+  코드를 읽기 힘들어지니까요.`;
+console.log(longString);
