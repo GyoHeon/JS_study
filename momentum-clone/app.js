2d48a3da71d13b7f09491f3048582680027fde27
GyoHeon Lee <ghl12078@gmail.com>
2021-11-12 20:32:03 +0900

    course 3.4

--- momentum-clone/app.js
+++ momentum-clone/app.js
@@ -4,6 +4,15 @@ function handleTitleClick() {
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "mouse is here!";
+}
+function handleMouseLeave() {
+  title.innerText = "mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
 
 console.dir(title);
