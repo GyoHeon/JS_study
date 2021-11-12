8ed5fba8a50b61de498c0a2c03cab7d7afdbe5d0
GyoHeon Lee <ghl12078@gmail.com>
2021-11-13 00:53:54 +0900

    course 3.6

--- momentum-clone/app.js
+++ momentum-clone/app.js
@@ -1,24 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleh1Click() {
-  h1.style.color = "blue";
-}
-function handleMouseEnter() {
-  h1.innerText = "mouse is here!";
-}
-function handleMouseLeave() {
-  h1.innerText = "mouse is gone!";
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "grey";
-}
-function handleWindowCopy() {
-  alert("copier!");
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
