84964202008793dd8fa4ab088ff667c5329bc63e
GyoHeon Lee <ghl12078@gmail.com>
2021-11-13 14:42:12 +0900

    course 4.3

--- momentum-clone/app.js
+++ momentum-clone/app.js
@@ -1,10 +1,19 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 
-function onLoginSubmit(data) {
-  data.preventDefault();
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
   //const username = loginInput.value;
-  console.log(data);
+  console.log(event);
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+  alert("clicked!");
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
